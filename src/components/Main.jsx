0a628cce5aa3ba9cc5774ac84e01e4213b041a86
Agnieszka-Dzwolak <dzwolak.agnieszka@gmail.com>
2024-05-30 23:07:44 +0200

    initial commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,41 @@
+import propTypes from "prop-types";
+import { useState } from "react";
+
+import Form from "./Form";
+import TodoList from "./TodoList";
+
+import "./Main.css";
+
+const Main = () => {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todoData) => {
+    setTodos((prev) => [todoData, ...prev]);
+  };
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
+
+  const checkTodo = (id) => {
+    setTodos((prev) =>
+      prev.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed: !todo.completed };
+        }
+        return todo;
+      })
+    );
+  };
+
+  return (
+    <div className="main-container">
+      <Form addTodo={addTodo} />
+      <TodoList todos={todos} deleteTodo={deleteTodo} checkTodo={checkTodo} />
+    </div>
+  );
+};
+
+Main.propTypes = {};
+
+export default Main;
